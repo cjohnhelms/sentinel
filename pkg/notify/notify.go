1479becfb296e87bdc8d1bdcea12d4beaa5176e1
Christopher Helms <cjohnhelms@gmail.com>
2024-12-21 15:17:20 -0600

    lcd screen support added

--- pkg/notify/notify.go
+++ pkg/notify/notify.go
@@ -1,68 +1,70 @@
 package notify
 
 import (
-	"errors"
 	"fmt"
 	"log"
-	"net/smtp"
 	"sentinel/pkg/scraper"
 	"time"
+
+	"github.com/twilio/twilio-go"
+	api "github.com/twilio/twilio-go/rest/api/v2010"
 )
 
-func SendMail(body string, recipient string) error {
-	from := "cjohnhelms@gmail.com"
-	pass := "dnax gtze bvvy fbao"
-	to := recipient
+func SendText(event scraper.Event, recipient string) error {
+	b := fmt.Sprint("AAC Event: %s\nStart time: %s", event.Title, event.Start)
+	client := twilio.NewRestClient()
 
-	msg := "From: " + from + "\n" +
-		"To: " + to + "\n" +
-		"Subject: Sentinel Report\n\n" +
-		body
+	params := &api.CreateMessageParams{}
+	params.SetBody(b)
+	params.SetFrom("+15127069578")
+	params.SetTo(recipient)
 
-	err := smtp.SendMail("smtp.gmail.com:587",
-		smtp.PlainAuth("", from, pass, "smtp.gmail.com"),
-		from, []string{to}, []byte(msg))
+	resp, err := client.Api.CreateMessage(params)
 	if err != nil {
-		return errors.New("smtp error: " + err.Error())
+		log.Println(err)
+	} else {
+		if resp.Body != nil {
+			fmt.Println(*resp.Body)
+		} else {
+			fmt.Println(resp.Body)
+		}
 	}
-	log.Println("Successfully sent to " + to)
 	return nil
 }
 
-func Notify(ch <-chan []scraper.Event, recipients [2]string) {
+func Notify(ch <-chan scraper.Event, recipients [2]string) {
 	for {
 		// Get the current time
 		now := time.Now()
 
 		// Calculate the next 2 PM
-		next2PM := time.Date(now.Year(), now.Month(), now.Day(), 14, 0, 0, 0, now.Location())
-		if now.After(next2PM) {
+		nextNotify := time.Date(now.Year(), now.Month(), now.Day(), 14, 0, 0, 0, now.Location())
+
+		if now.After(nextNotify) {
 			// If it’s already past 2 PM, schedule it for the next day
-			next2PM = next2PM.Add(24 * time.Hour)
+			nextNotify = nextNotify.Add(24 * time.Hour)
 		}
 
 		// Calculate the duration until the next 2 PM
-		duration := next2PM.Sub(now)
+		duration := nextNotify.Sub(now)
 
 		// Sleep until the next 2 PM
 		time.Sleep(duration)
 
-		today := time.Now().Format("2006-01-02")
 		select {
 		case data := <-ch:
-			for _, event := range data {
-				if event.Date == today {
-					body := fmt.Sprintf("\n%s - %s\nUse Harry Hines Blvd", event.Title, event.Start)
-					for _, recipient := range recipients {
-						err := SendMail(body, recipient)
-						if err != nil {
-							log.Println("Error sending notification: " + err.Error())
-						}
+			event := data
+			if event.Title == "" {
+				log.Println("no event found today")
+			} else {
+				for _, recipient := range recipients {
+					if err := SendText(event, recipient); err != nil {
+						log.Println(err)
 					}
 				}
 			}
 		default:
-			log.Println("No events in the channel")
+			log.Println("no events in the channel, something might be wrong")
 		}
 	}
 }
