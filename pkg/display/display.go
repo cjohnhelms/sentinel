d1a7ef34b3978ddc41f8fb593ec692a1051d4833
Christopher Helms <cjohnhelms@gmail.com>
2024-12-22 22:02:18 -0600

    overnight testing

--- pkg/display/display.go
+++ pkg/display/display.go
@@ -23,17 +23,17 @@ func Write(event scraper.Event) {
 	if len(event.Title) <= 16 {
 		screen.Print(1, 0, event.Title)
 	} else {
+		var i int
+		var max = len(event.Title) - 15
 		for {
-			var i int
-			var max = len(event.Title) - 15
 			if err := screen.Print(1, 0, event.Title); err != nil {
 				log.Println("screen update failure:", err)
 			}
-			time.Sleep(600 * time.Millisecond)
+			time.Sleep(800 * time.Millisecond)
 			i++
 			if i == max {
 				i = 0
-				time.Sleep(2 * time.Second)
+				time.Sleep(3 * time.Second)
 			}
 		}
 	}
@@ -41,21 +41,21 @@ func Write(event scraper.Event) {
 
 func Update(ch <-chan scraper.Event) {
 	for {
-		//// Get the current time
-		//now := time.Now()
+		// Get the current time
+		now := time.Now()
 
-		//// Calculate the next 2 PM
-		//nextUpdate := time.Date(now.Year(), now.Month(), now.Day(), 3, 0, 0, 0, now.Location())
-		//if now.After(nextUpdate) {
-		//	// If it’s already past 2 PM, schedule it for the next day
-		//	nextUpdate = nextUpdate.Add(24 * time.Hour)
-		//}
+		// Calculate the next 2 PM
+		nextUpdate := time.Date(now.Year(), now.Month(), now.Day(), 3, 0, 0, 0, now.Location())
+		if now.After(nextUpdate) {
+			// If it’s already past 2 PM, schedule it for the next day
+			nextUpdate = nextUpdate.Add(24 * time.Hour)
+		}
 
-		//// Calculate the duration until the next 2 PM
-		//duration := nextUpdate.Sub(now)
+		// Calculate the duration until the next 2 PM
+		duration := nextUpdate.Sub(now)
 
-		//// Sleep until the next 2 PM
-		//time.Sleep(duration)
+		// Sleep until the next 2 PM
+		time.Sleep(duration)
 
 		select {
 		case data := <-ch:
