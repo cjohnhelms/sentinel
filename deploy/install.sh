995cbb498a16de5fc924a5340c0d539d28809bdd
Christopher Helms <cjohnhelms@gmail.com>
2025-01-05 17:59:19 -0600

    typo fix

--- deploy/install.sh
+++ deploy/install.sh
@@ -6,7 +6,7 @@ sudo apt install -y wget > /dev/null
 
 # install golang
 wget -c https://golang.org/dl/go1.23.4.linux-arm64.tar.gz
-rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.23.4.linux-amd64.tar.gz
+rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.23.4.linux-arm64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
 # check
