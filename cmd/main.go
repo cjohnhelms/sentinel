8f339df0132fef80e94a0ba102643fd8374c723e
Christopher Helms <cjohnhelms@gmail.com>
2025-01-10 21:05:43 -0600

    temp disable LCD

--- cmd/main.go
+++ cmd/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 
 	"github.com/cjohnhelms/sentinel/pkg/config"
-	"github.com/cjohnhelms/sentinel/pkg/display"
 	"github.com/cjohnhelms/sentinel/pkg/notify"
 	"github.com/cjohnhelms/sentinel/pkg/scraper"
 
@@ -20,7 +19,7 @@ func main() {
 	ch := make(chan scraper.Event, 1)
 	go scraper.FetchEvents(ch)
 	go notify.Notify(ch, cfg)
-	go display.Update(ch)
+	//go display.Update(ch)
 
 	select {}
 }
