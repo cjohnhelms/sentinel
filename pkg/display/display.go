efbab197f28518acda675f18b7ae6b00c3125677
Christopher Helms <cjohnhelms@gmail.com>
2025-01-22 20:42:26 -0600

    Revert "trying again"
    This reverts commit 042af4c1fd6260b7906f8592c75b755f19db5748.

--- pkg/display/display.go
+++ pkg/display/display.go
@@ -46,9 +46,9 @@ func writeScreen(event scraper.Event, quit <-chan bool) {
 				t := event.Title + strings.Repeat(" ", r)
 				screen.Print(1, 0, t)
 			} else {
-				var moves = len(event.Title) - 15
-				for i := 0; i < moves; i++ {
-					if err := screen.Print(1, 0, event.Title[i:i+16]); err != nil {
+				var max = len(event.Title) - 15
+				for i := 0; i < max; i++ {
+					if err := screen.Print(1, 0, event.Title[i:(i+15)]); err != nil {
 						log.Error(fmt.Sprintf("Screen update failure: %s", err), "SERIVCE", "DISPLAY")
 					}
 					time.Sleep(800 * time.Millisecond)
