d8733ed953aa355dea664331e9724bb85f73485c
Christopher Helms <cjohnhelms@gmail.com>
2025-01-22 20:24:31 -0600

    less than?

--- pkg/display/display.go
+++ pkg/display/display.go
@@ -47,7 +47,7 @@ func writeScreen(event scraper.Event, quit <-chan bool) {
 				screen.Print(1, 0, t)
 			} else {
 				var max = len(event.Title) - 15
-				for i := 0; i <= max; i++ {
+				for i := 0; i < max; i++ {
 					if err := screen.Print(1, 0, event.Title[i:(i+16)]); err != nil {
 						log.Error(fmt.Sprintf("Screen update failure: %s", err), "SERIVCE", "DISPLAY")
 					}
