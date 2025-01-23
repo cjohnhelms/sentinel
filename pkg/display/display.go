4be24d4dc50f0996717e129938c0b1e9f5302c3a
Christopher Helms <cjohnhelms@gmail.com>
2025-01-22 19:56:45 -0600

    testing waitgroup sync

--- pkg/display/display.go
+++ pkg/display/display.go
@@ -1,8 +1,10 @@
 package display
 
 import (
+	"context"
 	"fmt"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/cjohnhelms/sentinel/pkg/scraper"
@@ -17,7 +19,7 @@ func writeTitle(event scraper.Event, screen *lcd.LCD, quit <-chan bool) {
 	for {
 		select {
 		case <-quit:
-			log.Info("New event found and quit recieved, killing goroutine")
+			log.Info("Quit recieved, killing goroutine")
 			return
 		default:
 			if err := screen.Print(1, 0, event.Title[i:(i+16)]); err != nil {
@@ -33,37 +35,45 @@ func writeTitle(event scraper.Event, screen *lcd.LCD, quit <-chan bool) {
 	}
 }
 
-func Update(ch <-chan scraper.Event, quit <-chan bool) {
+func Update(ctx context.Context, wg *sync.WaitGroup, ch <-chan scraper.Event, quit chan bool) {
+	defer wg.Done()
 	for {
-		time.Sleep(60 * time.Second)
+		select {
+		case <-ctx.Done():
+			log.Info("Killing display routine and children")
+			quit <- true
+			return
+		default:
+			time.Sleep(60 * time.Second)
 
-		event := <-ch
-		screen := lcd.New(lcd.LCD{Bus: "/dev/i2c-1", Address: 0x27, Rows: 2, Cols: 16, Backlight: true})
-		log.Debug(fmt.Sprintf("Screen: %+v", screen), "SERVICE", "DISPLAY")
+			event := <-ch
+			screen := lcd.New(lcd.LCD{Bus: "/dev/i2c-1", Address: 0x27, Rows: 2, Cols: 16, Backlight: true})
+			log.Debug(fmt.Sprintf("Screen: %+v", screen), "SERVICE", "DISPLAY")
 
-		if err := screen.Init(); err != nil {
-			log.Error("Failed to init screen, proceeding with SMS", "SERVICE", "DISPLAY")
-		}
-
-		log.Debug(fmt.Sprintf("Writing screen: %s - %s", event.Title, event.Start), "SERVICE", "DISPLAY")
-
-		// write time first because this is static
-		if event.Start != "" {
-			if err := screen.Print(2, 0, event.Start); err != nil {
-				log.Error(fmt.Sprintf("Screen update failure: %s", err), "SERVICE", "DISPLAY")
+			if err := screen.Init(); err != nil {
+				log.Error("Failed to init screen, proceeding with SMS", "SERVICE", "DISPLAY")
 			}
-		} else {
-			if err := screen.Print(2, 0, strings.Repeat(" ", 16)); err != nil {
-				log.Error(fmt.Sprintf("Screen update failure: %s", err), "SERVICE", "DISPLAY")
-			}
-		}
 
-		if len(event.Title) <= 16 {
-			r := (16 - len(event.Title))
-			t := event.Title + strings.Repeat(" ", r)
-			screen.Print(1, 0, t)
-		} else {
-			go writeTitle(event, screen, quit)
+			log.Debug(fmt.Sprintf("Writing screen: %s - %s", event.Title, event.Start), "SERVICE", "DISPLAY")
+
+			// write time first because this is static
+			if event.Start != "" {
+				if err := screen.Print(2, 0, event.Start); err != nil {
+					log.Error(fmt.Sprintf("Screen update failure: %s", err), "SERVICE", "DISPLAY")
+				}
+			} else {
+				if err := screen.Print(2, 0, strings.Repeat(" ", 16)); err != nil {
+					log.Error(fmt.Sprintf("Screen update failure: %s", err), "SERVICE", "DISPLAY")
+				}
+			}
+
+			if len(event.Title) <= 16 {
+				r := (16 - len(event.Title))
+				t := event.Title + strings.Repeat(" ", r)
+				screen.Print(1, 0, t)
+			} else {
+				go writeTitle(event, screen, quit)
+			}
 		}
 	}
 }
--- pkg/notify/notify.go
+++ pkg/notify/notify.go
@@ -1,9 +1,11 @@
 package notify
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"net/smtp"
+	"sync"
 	"time"
 
 	"github.com/cjohnhelms/sentinel/pkg/config"
@@ -37,44 +39,51 @@ func (em *Email) Send() error {
 	return nil
 }
 
-func Notify(ch <-chan scraper.Event, cfg *config.Config) {
+func Notify(ctx context.Context, wg *sync.WaitGroup, ch <-chan scraper.Event, cfg *config.Config) {
+	defer wg.Done()
 	for {
-		// Get the current time
-		now := time.Now()
+		select {
+		case <-ctx.Done():
+			log.Info("Killing notify routine")
+			return
+		default:
+			// Get the current time
+			now := time.Now()
 
-		// Calculate the next 2 PM
-		nextNotify := time.Date(now.Year(), now.Month(), now.Day(), 15, 0, 0, 0, now.Location())
+			// Calculate the next 2 PM
+			nextNotify := time.Date(now.Year(), now.Month(), now.Day(), 15, 0, 0, 0, now.Location())
 
-		if now.After(nextNotify) {
-			// If it’s already past 2 PM, schedule it for the next day
-			nextNotify = nextNotify.Add(24 * time.Hour)
-		}
-
-		// Calculate the duration until the next 2 PM
-		duration := nextNotify.Sub(now)
-
-		// Sleep until the next 2 PM
-		time.Sleep(duration)
-
-		event := <-ch
-		today := time.Now().Format("2006-01-02")
-		if event.Date == today {
-			log.Info(fmt.Sprintf("Sending emails to: %v", cfg.Emails), "SERVICE", "NOTIFY")
-			for _, recipient := range cfg.Emails {
-				m := &Email{
-					FromName:  "Sentinel",
-					FromEmail: cfg.Sender,
-					Password:  cfg.Password,
-					ToEmail:   recipient,
-					Subject:   "Sentinel Report",
-					Message:   fmt.Sprintf("AAC Event: %s - %s\n\nConsider alternate routes. Recommended to approach via Harry Hines Blvd.", event.Title, event.Start),
-				}
-				if err := m.Send(); err != nil {
-					log.Error(err.Error(), "SERVICE", "NOTIFY")
-				}
+			if now.After(nextNotify) {
+				// If it’s already past 2 PM, schedule it for the next day
+				nextNotify = nextNotify.Add(24 * time.Hour)
 			}
-		} else {
-			log.Info("No event today", "SERIVCE", "NOTIFY")
+
+			// Calculate the duration until the next 2 PM
+			duration := nextNotify.Sub(now)
+
+			// Sleep until the next 2 PM
+			time.Sleep(duration)
+
+			event := <-ch
+			today := time.Now().Format("2006-01-02")
+			if event.Date == today {
+				log.Info(fmt.Sprintf("Sending emails to: %v", cfg.Emails), "SERVICE", "NOTIFY")
+				for _, recipient := range cfg.Emails {
+					m := &Email{
+						FromName:  "Sentinel",
+						FromEmail: cfg.Sender,
+						Password:  cfg.Password,
+						ToEmail:   recipient,
+						Subject:   "Sentinel Report",
+						Message:   fmt.Sprintf("AAC Event: %s - %s\n\nConsider alternate routes. Recommended to approach via Harry Hines Blvd.", event.Title, event.Start),
+					}
+					if err := m.Send(); err != nil {
+						log.Error(err.Error(), "SERVICE", "NOTIFY")
+					}
+				}
+			} else {
+				log.Info("No event today", "SERIVCE", "NOTIFY")
+			}
 		}
 	}
 }
