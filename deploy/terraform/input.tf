26414cd27d92b6c7fbb2e312cb5ccdc4db0be4c0
Christopher Helms <cjohnhelms@gmail.com>
2025-07-25 15:24:36 -0500

    adding in secret handling

--- deploy/terraform/input.tf
+++ deploy/terraform/input.tf
@@ -2,3 +2,24 @@ variable "githubSHA" {
   type        = string
   description = "githubSHA"
 }
+
+variable "email_recipients" {
+  type        = string
+  description = "email recipients"
+}
+
+variable "email_server_password" {
+  type        = string
+  description = "email_server_password"
+}
+
+variable "service_email" {
+  type        = string
+  description = "service email"
+}
+
+variable "email_server" {
+  type        = string
+  description = "email server"
+}
+
