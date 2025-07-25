9658d158b296afbe625308686b780efd0364b304
Christopher Helms <cjohnhelms@gmail.com>
2025-07-25 16:17:04 -0500

    fix workflows and tests

--- src/pkg/email/email_test.go
+++ src/pkg/email/email_test.go
@@ -1,22 +1,50 @@
 package email
 
 import (
-	"github.com/cjohnhelms/sentinel/pkg/config"
+	"errors"
 	"github.com/cjohnhelms/sentinel/pkg/event"
+	"os"
+	"strings"
 	"testing"
 	"time"
 )
 
-func TestEmail_Send(t *testing.T) {
-	cfg, err := config.NewConfig()
-	if err != nil {
-		t.Fatal(err)
+func initialize() (map[string]string, error) {
+	rc, ok := os.LookupEnv("EMAIL_RECIPIENTS")
+	if !ok {
+		return nil, errors.New("EMAIL_RECIPIENTS environment variable not set")
 	}
+	es, ok := os.LookupEnv("EMAIL_SERVER")
+	if !ok {
+		return nil, errors.New("EMAIL_SERVER environment variable not set")
+	}
+	esp, ok := os.LookupEnv("EMAIL_SERVER_PASSWORD")
+	if !ok {
+		return nil, errors.New("EMAIL_SERVER_PASSWORD environment variable not set")
+	}
+	se, ok := os.LookupEnv("SERVICE_EMAIL")
+	if !ok {
+		return nil, errors.New("SERVICE_EMAIL environment variable not set")
+	}
+	return map[string]string{
+		"email_recipients":      rc,
+		"email_server":          es,
+		"email_server_password": esp,
+		"email_service":         se,
+	}, nil
+}
+
+func TestEmail_Send(t *testing.T) {
+	vars, err := initialize()
+	if err != nil {
+		t.Fatalf("failed to initialize: %s", err)
+	}
+
 	e := &Emails{
-		Recipient: cfg.RecipientEmails,
-		Server:    cfg.EmailServer,
-		Sender:    cfg.ServiceEmail,
-		Password:  cfg.EmailServerPassword,
+		Recipient: strings.Split(vars["email_recipients"], ","),
+		Server:    vars["email_server"],
+		Sender:    vars["email_server_password"],
+		Password:  vars["email_service"],
 	}
 	events := []event.Event{
 		{
